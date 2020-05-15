0a76c4d7761eb8d3519c9ecc85f399aaf73c65df
AlanisYates <yatesalanis@gmail.com>
2020-05-14 21:26:07 -0400

    Setup the found nad notfound errors middlewares

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,43 @@
 const express = require("express");
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
 
 const app = express();
-app.use(morgan('common'));
+
+//Middlewares
+app.use(morgan("common"));
 app.use(helmet());
-app.use(cors({
-    origin: 'http://localhost:3000',
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+// Error handeling middleware
+app.get("/", (req, res) => {
+  res.json({
+    medssage: "Hello world",
+  });
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`Not found = ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? 'Not avialible fam' : error.stack,
+  });
+});
 
 const port = process.env.PORT || 1337;
 
 app.listen(port, () => {
   console.log(`Listining at http://localhost:${port}`);
 });
-
