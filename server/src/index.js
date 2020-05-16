df3610d7c972c3055daf129bd61d36818bb745a6
AlanisYates <yatesalanis@gmail.com>
2020-05-16 19:07:09 -0400

    set up the mongoose routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,41 +1,43 @@
-const express = require("express");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-const middlewares = require("./middlewares");
+const middlewares = require('./middlewares');
+const logs = require('./api/logs');
 
 const app = express();
 
+// app.enable('trust proxy'); // needed for rate limiting by Client IP
+
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-
-app.use(morgan("common"));
+app.use(morgan('common'));
 app.use(helmet());
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-  })
-);
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+}));
 
-// Error handeling middleware
-app.get("/", (req, res) => {
+app.use(express.json());
+
+app.get('/', (req, res) => {
   res.json({
-    medssage: "Hello world",
+    message: 'Hello World!',
   });
 });
 
-app.use(middlewares.notFound);
+app.use('/api/logs', logs);
 
+app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 1337;
-
 app.listen(port, () => {
-  console.log(`Listining at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
--- server/src/models/LogEntry.js
+++ server/src/models/LogEntry.js
@@ -1,48 +1,44 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
 
-const requiredString = {
-  type: String,
-  required: true,
-};
+const { Schema } = mongoose;
 
 const requiredNumber = {
   type: Number,
   required: true,
 };
 
-var logEntrySchema = new Schema(
-  {
-    title: requiredString,
-    description: String,
-    comments: String,
-    image: String,
-    rating: {
-      Number,
-      min: 0,
-      max: 10,
-      default: 0,
-    },
-    latitude: {
-      ...requiredNumber,
-      min: -90,
-      max: 90,
-    },
-    longitude: {
-      ...requiredNumber,
-      min: -180,
-      max: 180,
-    },
-    visitDate: {
-      required: true,
-      type: Date,
-    },
+const logEntrySchema = new Schema({
+  title: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  description: String,
+  comments: String,
+  image: String,
+  rating: {
+    type: Number,
+    min: 0,
+    max: 10,
+    default: 0,
+  },
+  latitude: {
+    ...requiredNumber,
+    min: -90,
+    max: 90,
+  },
+  longitude: {
+    ...requiredNumber,
+    min: -180,
+    max: 180,
+  },
+  visitDate: {
+    required: true,
+    type: Date,
+  },
+}, {
+  timestamps: true,
+});
 
-const LogEntry = mongoose.model("LogEntry", logEntrySchema);
+const LogEntry = mongoose.model('LogEntry', logEntrySchema);
 
 module.exports = LogEntry;
