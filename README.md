4e9dc927bb63b644367c8fb9441e333c2706e66e
AlanisYates <yatesalanis@gmail.com>
2020-05-15 23:00:52 -0400

    created the log Schema in mongoose

--- README.md
+++ README.md
@@ -5,9 +5,8 @@
 * Personal Comments -Text
 * Rating -scale 1 - 10
 * Image - Text - URL
-* Start Date - DateTime
-* End Date - DateTime
 * Latitude - Number
+* Date Vidited - DateTime
 * Longitude - Number
 * Created At - DateTime
 * Updated At - DateTime
---
+++ server/src/models/LogEntry.js
@@ -0,0 +1,36 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+const requiredString = {
+  type: String,
+  required: true,
+};
+
+const requiredNumber = {
+  type: Number,
+  required: true,
+};
+
+var logEntrySchema = new Schema(
+  {
+    title: requiredString,
+    description: String,
+    comments: String,
+    image: String,
+    rating: {
+      Number,
+      min: 0,
+      max: 10,
+      default: 0,
+    },
+    latitude: requiredNumber,
+    longitude: requiredNumber,
+    visitDate: {
+      required: true,
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
