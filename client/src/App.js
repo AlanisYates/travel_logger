11132a70b2bea810e31596291f1f30691951071f
AlanisYates <yatesalanis@gmail.com>
2020-05-16 22:26:19 -0400

    FIxing the db

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
-import * as React from "react";
-import { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+
+import { listLogEntries } from "./API";
 
 const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
   const [viewport, setViewport] = useState({
-    width: '100vw',
-    height: '100vh',
+    width: "100vw",
+    height: "100vh",
     latitude: 36.6,
     longitude: -95.656,
     zoom: 3,
   });
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      setLogEntries(logEntries);
+      console.log(logEntries);
+    })();
+  }, []);
 
   return (
     <ReactMapGL
@@ -17,7 +27,19 @@ const App = () => {
       mapStyle="mapbox://styles/thecjreynolds/ck117fnjy0ff61cnsclwimyay"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
-    />
+    >
+      {logEntries.map((entry) => (
+        <Marker
+          key={entry._id}
+          latitude={entry.latitude}
+          longitude={entry.longitude}
+          offsetLeft={-20}
+          offsetTop={-10}
+        >
+          <div>You are here</div>
+        </Marker>
+      ))}
+    </ReactMapGL>
   );
 };
 
